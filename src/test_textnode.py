65aca5965199b44fae5b7bd83734f77d124cb9d7
shahinyahya <shahinyahya@gmail.com>
2025-10-27 11:52:47 +0530

    added first tesy

---
+++ src/test_textnode.py
@@ -0,0 +1,19 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is test node 1", TextType.BOLD)
+        node2 = TextNode("This is test node 2", TextType.ITALIC)
+    
+        node3 = TextNode("img_text", TextType.IMAGE, "adsg")
+        node4 = TextNode("link", TextType.LINK, "dfa")
+
+        self.assertEqual(node, node2)
+        # self.assertNotEqual(node, node2)
+        # self.assertNotEqual(node3, node4)
+
+
+if __name__ == "__main__":
+    unittest.main()
